1bc8d7c1f0d7d190cc313f2362191f7615718d79
Zargon2019 <51010112+Zargon2019@users.noreply.github.com>
2021-03-01 11:05:31 -0500

    Update guessingGame.js

--- Scripts/guessingGame.js
+++ Scripts/guessingGame.js
@@ -31,8 +31,8 @@ function checkGuess() {
     lastResult.textContent = 'Wrong!';
     lastResult.style.backgroundColor = 'red';
     if(userGuess < randomNumber) {
-      lowOrHi.textContent = 'Guess number ' + guessCount + '\n';
-	lowOrHi.textContent += '  Last guess was too low!';
+      lowOrHi.textContent = 'Guess number ' + guessCount;
+	lowOrHi.textContent += '\nLast guess was too low!';
     } else if(userGuess > randomNumber) {
       lowOrHi.textContent = 'Guess number ' + guessCount + ' Last guess was too high!';
     }
